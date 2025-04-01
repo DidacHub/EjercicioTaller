4fe5986e260462c3ef8bb484825b24e370116e4f
DidacHub <56065397+DidacHub@users.noreply.github.com>
2025-04-01 10:11:56 +0000

    Arreglado insert facturas

--- Solución_Taller/Solución_Taller/Inserts/08_Inert_Facturas.sql
+++ Solución_Taller/Solución_Taller/Inserts/08_Inert_Facturas.sql
@@ -1,5 +1,5 @@
 
-INSERT INTO Factura (Fecha, NIF, Matrícula, Kilometraje, ImporteSinIVA, ImporteIVA) VALUES
-('2023-11-25', '12345678A', 'AAA1111', 50500, 180.00, 217.80),
-('2023-11-28', '98765432B', 'BBB2222', 75500, 350.00, 423.50);
+INSERT INTO Factura (Fecha, NIF, Matrï¿½cula, Kilometraje, ImporteSinIVA, ImporteIVA) VALUES
+('2025-01-18', '12345678A', 'AAA1111', 50500, 180.00, 217.80),
+('2025-02-13', '98765432B', 'BBB2222', 75500, 350.00, 423.50);
 
